6ed4e1bc9b3b1ae83421acf94a378efd65f792ca
SurabhiYadav1 <s.yadav@gembainfotech.com>
2024-09-12 11:18:55 +0530

    apiBaseUrl

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,7 +1,7 @@
 const appConfig = {
-  apiBaseUrl: "http://surabhi:8000/",
+  apiBaseUrl: "https://api.bracetekk.com/",
   apiPrefix: "/api",
-  imageBaseUrl: "http://surabhi:8000/bucket/",
+  imageBaseUrl: "https://api.bracetekk.com/bucket/",
   authenticatedEntryPath: "/app/dashboard",
   unAuthenticatedEntryPath: "/sign-in",
   tourPath: "/app/account/kyc-form",
