7136141bec43be95db7a7adec125aa32d8650ed1
SurabhiYadav1 <s.yadav@gembainfotech.com>
2025-02-21 13:32:49 +0530

    dropdown added for the Device Type

--- src/views/masters/typesOfDevices/addEdit/index.js
+++ src/views/masters/typesOfDevices/addEdit/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { AdaptableCard } from "components/shared";
 import {
   Button,
@@ -8,12 +8,14 @@ import {
   FormContainer,
   toast,
   Notification,
+  Select,
 } from "components/ui";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
-import { postApi } from "services/CommonService";
+import { postApi, getApi } from "services/CommonService";
 import { APIS, LIST_DATA_API_TYPE } from "constants/api.constant";
 import { AiOutlineSave, AiOutlineCloseCircle } from "react-icons/ai";
+import { useSelector } from "react-redux";
 
 const DrawerFooter = ({ editData, onCancel, onSave, isLoading }) => {
   return (
@@ -45,21 +47,64 @@ const SignupSchema = Yup.object().shape({
 
 const initialValues = {
   name: "",
+  companyId: "",
 };
 
 const AddEditDeviceType = ({ editData, show, onClose, refreshPage }) => {
-  const savedHospitalId = localStorage.getItem("selectedHospitalId");
-
+  const [companyOptions, setCompanyOptionos] = useState([]);
+  const user = useSelector((state) => state.auth.user);
   const formRef = useRef();
   const [loading, setLoading] = useState(false);
 
-  const onSubmit = ({ name, id }) => {
+  useEffect(() => {
+    if (user?.companyId) {
+      getApi(APIS.LIST_DATA, {
+        companyIds: JSON.stringify(user.companyId),
+        type: LIST_DATA_API_TYPE.COMPANY,
+      })
+        .then((res) => {
+          const response = res?.data?.data;
+
+          if (Array.isArray(response)) {
+            const companyOptions = response.map((company) => ({
+              label: company.name,
+              value: company._id,
+            }));
+            setCompanyOptionos(companyOptions);
+          }
+        })
+        .catch((error) => {
+          // Handle any errors from the additional API call
+          console.error("Error calling additional API:", error);
+        });
+    } else {
+      getApi(APIS.LIST_DATA, { type: LIST_DATA_API_TYPE.COMPANY })
+        .then((res) => {
+          if (res && res.data && Array.isArray(res.data.data)) {
+            const locations = res.data.data.map((location) => ({
+              label: location.name,
+              value: location._id,
+            }));
+            setCompanyOptionos(locations);
+          } else {
+            toast.push(<Notification type="error">No Companies found!</Notification>);
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching Companies:", error);
+          toast.push(<Notification type="error">Failed to load Companies</Notification>);
+        });
+    }
+
+  }, []);
+
+  const onSubmit = ({ name, id, companyId }) => {
     setLoading(true);
     const payload = {
       name,
-      modelType : LIST_DATA_API_TYPE.DEVICE_TYPES,
-      companyId: savedHospitalId
-      
+      modelType: LIST_DATA_API_TYPE.DEVICE_TYPES,
+      companyId: companyId,
+
     };
 
     if (id) {
@@ -105,6 +150,28 @@ const AddEditDeviceType = ({ editData, show, onClose, refreshPage }) => {
           {({ errors, touched, setFieldValue, values }) => (
             <Form className="p-5">
               <FormContainer>
+                <FormItem
+                  label="Company"
+                  invalid={errors.companyId && touched.companyId}
+                  errorMessage={errors.companyId}
+                >
+                  <Field name="companyId">
+                    {({ field, form }) => (
+                      <Select
+                        {...field}
+                        options={companyOptions}
+                        placeholder="Select Company"
+                        value={companyOptions.find(
+                          (option) => option.value === field.value
+                        )}
+                        onChange={(selectedOption) =>
+                          form.setFieldValue("companyId", selectedOption.value)
+                        }
+                        onBlur={field.onBlur}
+                      />
+                    )}
+                  </Field>
+                </FormItem>
                 <FormItem
                   label="Device Type"
                   invalid={errors?.name && touched?.name}
