b5adbb88621cfbb7efcf64fb829fa8ec46b85a71
SurabhiYadav1 <s.yadav@gembainfotech.com>
2025-04-05 15:45:44 +0530

    backend with https

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,7 +1,7 @@
 const appConfig = {
-  apiBaseUrl: "http://103.171.96.154/api/",
+  apiBaseUrl: "https://103.171.96.154/api/",
   apiPrefix: "/api",
-  imageBaseUrl: "http://103.171.96.154/api/bucket/",
+  imageBaseUrl: "https://103.171.96.154/api/bucket/",
   authenticatedEntryPath: "/app/dashboard",
   unAuthenticatedEntryPath: "/sign-in",
   tourPath: "/app/account/kyc-form",
